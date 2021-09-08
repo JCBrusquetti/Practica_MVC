cc8cd3e9e66a14136be13cd2cea77f7995aa19a0
JCBrusquetti <josecarlosbrusquetti@hotmail.com>
2021-09-08 13:29:56 -0400

    Comprobacion de existencia de controladores y aacciones

--- index.php
+++ index.php
@@ -3,13 +3,20 @@
 <?php
 require_once 'controllers/UsuarioController.php';
 
-$controlador = new UsuarioController();
+if (isset($_GET['controller']) && class_exists($_GET['controller'])) {
+    $nombre_controller = $_GET['controller'];
+    $controlador = new $nombre_controller();
+    if (isset($_GET['action']) && method_exists($controlador,$_GET['action'])) {
+        $action = $_GET['action'];
+        $controlador->$action();
+    }else {
+        echo "La página que buscas no existe";
+    }
+}else {
+    echo "La página que buscas no existe";
+}
+// $controlador = new UsuarioController();
 // $controlador->mostrarTodos();
 
 // $controlador->crear();
 
-
-if (isset($_GET['action'])) {
-    $action = $_GET['action'];
-    $controlador->$action();
-}
